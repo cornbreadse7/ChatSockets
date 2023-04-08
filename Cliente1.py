d739cae6055c35003e67c5d59b544c4ace70873f
cornbreadse7 <80598656+cornbreadse7@users.noreply.github.com>
2023-04-08 17:00:35 -0500

    Codigo Inicial

---
+++ Cliente1.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+import tkinter as tk
+
+# Definir el host y el puerto en el que se ejecuta el servidor
+HOST = '127.0.0.1'
+PORT = 5000
+
+# Crear una ventana de chat
+root = tk.Tk()
+root.title('Chat - Cliente 1')
+
+# Crear una caja de entrada para el nombre de usuario
+username_label = tk.Label(root, text='Username:')
+username_label.pack(side=tk.LEFT)
+username_entry = tk.Entry(root)
+username_entry.pack(side=tk.LEFT)
+
+# Crear una caja de entrada para los mensajes
+message_label = tk.Label(root, text='Message:')
+message_label.pack(side=tk.LEFT)
+message_entry = tk.Entry(root)
+message_entry.pack(side=tk.LEFT)
+
+# Función para enviar un mensaje al servidor
+def send_message(event=None):
+    username = username_entry.get()
+    message = message_entry.get()
+    # Enviar el mensaje al servidor
+    client_socket.sendall(f'{username}: {message}'.encode())
+    # Borrar la caja de entrada del mensaje
+    message_entry.delete(0, tk.END)
+
+# Configurar el botón de enviar mensaje
+send_button = tk.Button(root, text='Send', command=send_message)
+send_button.pack(side=tk.LEFT)
+
+# Función para recibir mensajes del servidor
+def receive_messages():
+    while True:
+        # Recibir datos del servidor
+        data = client_socket.recv(1024)
+        # Decodificar los datos recibidos
+        message = data.decode()
+        # Agregar el mensaje a la caja de texto
+        chat_box.insert(tk.END, f'{message}\n')
+
+# Conectar al servidor
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+# Iniciar un hilo para recibir mensajes del servidor
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
+
+# Crear una caja de texto para mostrar los mensajes
+chat_box = tk.Text(root)
+chat_box.pack()
+
+# Iniciar la ventana de chat
+root.mainloop()
+
+# Cerrar la conexión del cliente
+client_socket.close()
---
+++ Servidor.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+
+# Definir el host y el puerto en el que se ejecutará el servidor
+HOST = '127.0.0.1'
+PORT = 5000
+
+# Crear un socket del servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Enlazar el socket del servidor al host y puerto especificados
+server_socket.bind((HOST, PORT))
+
+# Escuchar nuevas conexiones entrantes
+server_socket.listen()
+
+# Lista para almacenar los clientes conectados
+clients = []
+
+# Función para enviar mensajes a todos los clientes
+def broadcast(message, sender):
+    for client in clients:
+        # Enviar el mensaje a todos los clientes, excepto al que lo envió
+        if client != sender:
+            client.sendall(message)
+
+# Función para manejar la conexión de un cliente
+def handle_client(client_socket, client_address):
+    print(f'Nuevo cliente conectado: {client_address}')
+    # Agregar el cliente a la lista de clientes
+    clients.append(client_socket)
+
+    # Loop para recibir y enviar mensajes
+    while True:
+        # Recibir el mensaje del cliente
+        data = client_socket.recv(1024)
+        if not data:
+            # Si no hay datos, se rompe el bucle
+            break
+        # Enviar el mensaje a todos los demás clientes
+        broadcast(data, client_socket)
+
+    # Cerrar la conexión del cliente
+    client_socket.close()
+    # Eliminar el cliente de la lista de clientes
+    clients.remove(client_socket)
+    print(f'Cliente desconectado: {client_address}')
+
+# Función para esperar nuevas conexiones
+def wait_for_connections():
+    while True:
+        # Esperar una nueva conexión entrante
+        client_socket, client_address = server_socket.accept()
+        # Iniciar un hilo para manejar la conexión del cliente
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
+
+# Iniciar un hilo para esperar nuevas conexiones
+connection_thread = threading.Thread(target=wait_for_connections)
+connection_thread.start()
+
